d553ff92f72ac7aab549288154f109ade455c5a1
d-leaf <346691836@qq.com>
2023-09-09 17:53:03 +0800

    feat: dynamic router

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,9 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
-import { sign_in_router, user_router } from '../router'
+import { register_routes, sign_in_router, user_router } from '../router'
 
 export const app = new Koa()
 
 app.use(bodyParser())
-app.use(user_router.routes())
-app.use(user_router.allowedMethods())
-app.use(sign_in_router.routes())
-app.use(sign_in_router.allowedMethods())
+
+register_routes(app)
--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,25 @@
-export * from './user'
-export * from './sign-in'
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+export * from './user.router'
+export * from './sign-in.router'
+
+export async function register_routes(app) {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+
+  const files = fs.readdirSync(__dirname)
+
+  for (const file of files) {
+    if (!file.endsWith('.router.js'))
+      continue
+
+    const module = await import(`./${file}`)
+    const router_key = Object.keys(module)[0]
+    const router = module[router_key]
+
+    app.use(router.routes())
+    app.use(router.allowedMethods())
+  }
+}
